f688bfaa0bb7daf65b8f88467214b605dbf5157e
Ambroise bareux <ambroise.bareux@gmail.com>
2024-12-15 00:14:44 +0100

    value transmitted trough the page

--- UwU/js/Game.js
+++ UwU/js/Game.js
@@ -1,7 +1,24 @@
-function displayMessage() {
-    if (document.getElementById("playerNumber").value < 128 && document.getElementById("playerNumber").value > 0) {
-        alert(document.getElementById("playerNumber").value);
+const input = document.getElementById("playerNumber");
+
+function launchGame() {
+    if (input.value <= 128 && input.value >= 1) {
+        window.location.href = `game.html?value=${input.value}`;
     } else {
         alert("Invalid number");
     }
 }
+
+function launchGameIfEnter(code) {
+    if (code === "Enter") {
+        launchGame();
+    }
+}
+
+function nextGame() {
+    const value = new URLSearchParams(window.location.search).get("value");
+
+    alert(value);
+}
+
+if (input != null)
+    input.addEventListener("keypress", function (event) {launchGameIfEnter(event.code)});
